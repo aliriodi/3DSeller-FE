8fe1fc47c3e07f4cf3a0d58e4a537c004779d8c4
Hugo Reyes <chugo940@gmail.com>
2023-01-04 14:23:30 -0600

    Se creo la search bar dentro del componente products

--- 3dseller/components/Products/Products.jsx
+++ 3dseller/components/Products/Products.jsx
@@ -1,33 +1,58 @@
-import React, { useEffect } from 'react'
-import {useDispatch, useSelector} from 'react-redux';
-import {getProducts} from '../../redux/DSellerActions';
-import Vcard from '../Vcard/Vcard'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../../redux/DSellerActions";
+import Vcard from "../Vcard/Vcard";
 
 function Products() {
-    const dispatch = useDispatch();
-    const {products} = useSelector(state=>state.products);
-    useEffect (()=>{dispatch(getProducts())
-   // eslint-disable-next-line
-    },[])
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+  console.log(products);
+  useEffect(() => {
+    dispatch(getProducts());
+    // eslint-disable-next-line
+  }, [dispatch]);
+
+  const productos = () => {
+    if (search !== "") {
+      const filtrados = products.filter((item) =>
+        item.name.toLowerCase().includes(search.toLocaleLowerCase())
+      );
+      return filtrados;
+    } else return products;
+  };
+
+  const [search, setSearch] = useState("");
+  const onSearchChange = ({ target }) => {
+    setSearch(target.value);
+  };
+
   return (
     <div>
-       
-        <div className='container0'>
+      <div className="container0">
+        <div>
+          <input
+            type="text"
+            placeholder="Busca tu producto"
+            value={search}
+            onChange={onSearchChange}
+          />
+        </div>
         <div className="flex-container">
-      {products &&  products.map(product3d=>{
-                return ( <Vcard 
-                    key={product3d.name}
-                    id={product3d.id}
-                    name={product3d.name}
-                    image={product3d.image}
-                     />        
-                    
-        ); }) }
-       
+          {products &&
+            productos().map((product3d) => {
+              return (
+                <Vcard
+                  key={product3d.name}
+                  id={product3d.id}
+                  name={product3d.name}
+                  image={product3d.image}
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
-   </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
